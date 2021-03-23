40c5ec045a268e7cfd6e522d0c8b8f6220e5f4ba
Daniel Niemann <danielhjermitslev@protonmail.com>
2021-03-23 22:33:24 +0100

    --tile section completed

--- src/components/TilesRight.js
+++ src/components/TilesRight.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { ReactComponent as MultiCurrencies } from '../img/multicurrys.svg'
+import { ReactComponent as MultiCurryImg } from '../img/multicurrys.svg'
+import { ReactComponent as TokensImg } from '../img/tokens_5.svg'
+import { ReactComponent as ExchangeImg } from '../img/exchange.svg'
 import './TilesRight.scss'
 
 export default function TilesRight() {
@@ -9,25 +11,40 @@ export default function TilesRight() {
         <h1 className="tiles__right--headline">
           one digital currency, multiple tokenized economies
       </h1>
-        <MultiCurrencies className="tiles__four--img" />
+        <MultiCurryImg className="tiles__four--img" />
         <p className="tiles__four--txt">
           Receive, send and store the most popular tokens, like
-      <br />
-      Shua, REX1, USDC and lots more.
+          <br />
+          Shua, REX1, USDC and lots more.
         </p>
       </div>
 
       <div className="tiles__five">
+        <h1 className="tiles__right--headline">
+          Tokens
+      </h1>
+        <TokensImg className="tiles__five--img" />
         <p className="tiles__five--txt">
+          Full support for any BitCoin tokens on the RUN network.
           <br />
+          Bitcoin tokens (SFPMB@0.1) is coming soon.
         </p>
 
       </div>
 
       <div className="tiles__six">
+        <h1 className="tiles__six--headline">
+          Exchange
+      </h1>
         <p className="tiles__six--txt">
-          <br />
+        In-app exchange is supported by Agern, it
+        <br />
+        allows to convert coins to each other in the most
+        <br />
+        intuitive and easiest way.
         </p>
+        <ExchangeImg className="tiles__six--img" />
+
       </div>
     </div>
   )
